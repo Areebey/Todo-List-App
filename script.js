ca8ba012cc970335e01672472cd043066583f125
Areebey <areebrafiq125@gmail.com>
2023-02-24 02:47:24 +0500

    second commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+const tittle = document.getElementById("tittle");
+const description = document.getElementById("description");
+const form = document.querySelector("form");
+const container = document.querySelector(".container"); 
+
+const task=[] // array of object
+
