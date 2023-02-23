eb32b7910c75b1eee2602e50065c31f70794d821
Areebey <areebrafiq125@gmail.com>
2023-02-24 03:28:33 +0500

    thr-commit

--- script.js
+++ script.js
@@ -5,5 +5,45 @@ const description = document.getElementById("description");
 const form = document.querySelector("form");
 const container = document.querySelector(".container"); 
 
-const task=[] // array of object
+const tasks = [] // array of object
 
+function showTasks(){
+    tasks.forEach((value,index)=>{
+        const div=document.createElement("div")
+        div.setAttribute("class","task");
+
+        const innerDiv=document.createElement("innerDiv");
+        div.append(innerDiv);
+
+        const p=document.createElement("p")
+        p.innerText=value.tittle;
+        innerDiv.append(p);
+
+        const span=document.createElement("span")
+        span.innerText=value.description;
+        innerDiv.append(span);
+
+        const btn=document.createElement("button")
+        btn.setAttribute("class","deletebtn")
+        btn.innerText="-";
+        div.append(btn);
+        container.append(div);
+    })
+}
+
+function removeTasks(){
+    tasks.forEach(()=>{
+    const div=document.querySelector(".task")
+    div.remove()
+})
+}
+
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    removeTasks();
+    tasks.push({
+        tittle: "Areeb",
+        description:"Complete your work",
+    })
+    showTasks();
+})
