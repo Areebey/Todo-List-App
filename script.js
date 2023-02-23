6e08be8f459488aa03bbda41a542aec516cfd7f5
Areebey <areebrafiq125@gmail.com>
2023-02-24 04:14:15 +0500

    final commit

--- script.js
+++ script.js
@@ -1,31 +1,43 @@
-
+//Todo list App
 
 const tittle = document.getElementById("tittle");
 const description = document.getElementById("description");
 const form = document.querySelector("form");
 const container = document.querySelector(".container"); 
 
-const tasks = [] // array of object
+// array of object
+const tasks = localStorage.getItem("tasks")
+?JSON.stringify(localStorage.getItem("tasks"))
+:[] ;
+
+showTasks();
 
 function showTasks(){
     tasks.forEach((value,index)=>{
-        const div=document.createElement("div")
+        const div=document.createElement("div");
         div.setAttribute("class","task");
 
-        const innerDiv=document.createElement("innerDiv");
+        const innerDiv=document.createElement("div");
         div.append(innerDiv);
 
-        const p=document.createElement("p")
+        const p=document.createElement("p");
         p.innerText=value.tittle;
         innerDiv.append(p);
 
-        const span=document.createElement("span")
+        const span=document.createElement("span");
         span.innerText=value.description;
         innerDiv.append(span);
 
-        const btn=document.createElement("button")
-        btn.setAttribute("class","deletebtn")
+        const btn=document.createElement("button");
+        btn.setAttribute("class","deletebtn");
         btn.innerText="-";
+
+        btn.addEventListener("click",()=>{
+            removeTasks()
+            tasks.splice(index,1);
+            localStorage.setItem("tasks",JSON.stringify(tasks));
+            showTasks();
+        })
         div.append(btn);
         container.append(div);
     })
@@ -33,17 +45,19 @@ function showTasks(){
 
 function removeTasks(){
     tasks.forEach(()=>{
-    const div=document.querySelector(".task")
-    div.remove()
+    const div=document.querySelector(".task");
+    div.remove();
 })
 }
 
 form.addEventListener("submit",(e)=>{
     e.preventDefault();
     removeTasks();
+
     tasks.push({
-        tittle: "Areeb",
-        description:"Complete your work",
-    })
+        tittle: tittle.value,
+        description:description.value,
+    });
+    localStorage.setItem("tasks",JSON.stringify(tasks));
     showTasks();
-})
+});
